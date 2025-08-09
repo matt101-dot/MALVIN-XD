df75d513939a433892b7283ac2ea2ed85437db03
matt101-dot <akhagbemhemathew@gmail.com>
2025-08-10 00:53:19 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~giMQnISY#vBOoXx3hHSi3k--68CmBjiLpnjGHaBL9yJJNIAEvO74",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "2349031929027", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
