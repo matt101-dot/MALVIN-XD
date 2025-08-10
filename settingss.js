72323baf184dafe99b72fe08b92db207735b2aba
matt101-dot <akhagbemhemathew@gmail.com>
2025-08-10 01:48:35 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "malvin~EqdHTazI#bGtPW30MO1DpsAmDdhUJ-O9IkVpi21c46c2dFzCGrwE ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "2348087205566", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~giMQnISY#vBOoXx3hHSi3k--68CmBjiLpnjGHaBL9yJJNIAEvO74 ",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "2349031929027", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
